292ef8c1df60dc2cc7d548becbe02155529dd1d3
Yurii Trofimenko <tyaa@ukr.net>
2018-07-13 16:26:46 +0300

    Добавьте файлы проекта.

---
+++ AsyncAwaitDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 43; i++)
+            {
+                long resultLong = GetFib(i).Result;
+                Console.WriteLine(resultLong);
+            }
+        }
+
+        static long Fib(long n) {
+
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+        async static Task<long> GetFib(long n) {
+
+            return Fib(n);
+        }
+    }
+}
